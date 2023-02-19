52cff8f67d0e543250bb494644452d059bda8bfa
pizukivan <73249115+pizukivan@users.noreply.github.com>
2023-02-19 13:55:20 -0800

    Add files via upload

---
+++ memoizedFibonacci.js
@@ -0,0 +1,9 @@
+const fibonacci = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n == 0 || n == 1)
+        return n;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+};
+
+console.log(fibonacci(10));
